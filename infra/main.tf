92b83cd021622b57ea5d445507811dec479bcede
Tu Nombre <andresyalca@gmail.com>
2025-06-29 13:37:29 -0500

    terraform no se puede conectar a github solo levantar la infra

--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.9"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
-resource "azurerm_static_web_app" "dev_action" {
+resource "azurerm_static_web_app" "dev_site" {
   name                = "webunoapp-dev"
   resource_group_name = "webuno_group"
   location            = "Central US"
   sku_tier            = "Free"
-
-
-  repository_url = "https://github.com/KamiALK/kamilo_static"
-  branch         = "dev"
-  build_properties = {
-    app_location          = "/"
-    api_location          = "api"
-    output_location       = "build"
-    app_artifact_location = "build"
-  }
-
 }
