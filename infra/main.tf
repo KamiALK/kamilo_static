8749b52a592493c5ad24c5f90f791bc9921de47b
Tu Nombre <andresyalca@gmail.com>
2025-06-30 14:39:31 -0500

    despues de la infra

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = ">=1.12.1"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -10,9 +11,35 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_static_web_app" "prod_site" {
+  name                = "webunoapp-prod"
+  resource_group_name = "webuno_group"
+  location            = "Central US"
+  sku_tier            = "Free"
+}
+
 resource "azurerm_static_web_app" "dev_site" {
   name                = "webunoapp-dev"
   resource_group_name = "webuno_group"
   location            = "Central US"
   sku_tier            = "Free"
+}
+
+output "deployment_token_dev" {
+  value     = azurerm_static_web_app.dev_site.api_key
+  sensitive = true
+}
+
+output "deployment_token_prod" {
+  value     = azurerm_static_web_app.prod_site.api_key
+  sensitive = true
+}
+
+output "hostname_dev" {
+  value = azurerm_static_web_app.dev_site.default_host_name
+}
+
+output "hostname_prod" {
+  value = azurerm_static_web_app.prod_site.default_host_name
 }
