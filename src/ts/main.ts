c04e5ecaf58fffcea9aca23d9da53eac21ad69f0
kamilo <andresyalca@gmail.com>
2025-09-21 23:57:26 -0500

    configuracion de html

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,11 @@
-console.log("hola camilito como estas");
+console.log("todo se encuentra fruncionando");
+document.addEventListener("DOMContentLoaded", () => {
+	const button = document.getElementById("test-bin");
+	const output = document.getElementById("output");
+
+	if (button && output) {
+		button.addEventListener("click", () => {
+			output.innerHTML = `<p>Typescript se encuentra contectado a  las ${new Date().toLocaleString()}</p>`;
+		});
+	}
+});
